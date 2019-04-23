800ff8083126fe8abfec427affaeccdc799a089a
Chris Joakim <christopher.joakim@gmail.com>
2019-04-23 17:36:14 -0400

    Updated python requirements per latest GitHub warnings

--- solutions/challenge9/python/venv.sh
+++ solutions/challenge9/python/venv.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
 # Recreate the virtual environment and reinstall libs.
-# Chris Joakim, Microsoft, 2019/04/13
+# Requires Python 3; version 3.7 or higher recommended.
+# Chris Joakim, Microsoft, 2019/04/23
 
 echo 'deleting previous venv...'
 rm -rf bin/
-rm -rf lib/
 rm -rf include/
+rm -rf lib/
 rm -rf man/
 
-echo 'creating new venv...'
+echo 'creating new venv ...'
 python3 -m venv .
 source bin/activate
 python --version
+pip --version
 
-echo 'installing/upgrading libs...'
+echo 'installing/upgrading pip...'
+pip install --upgrade pip
+pip --version
+
+echo 'installing/upgrading pip-tools...'
 pip install --upgrade pip-tools
 
-pip install --upgrade arrow
-pip install --upgrade azure
-pip install --upgrade docopt
-pip install --upgrade cql
-pip install --upgrade cassandra-driver
-pip install --upgrade prettytable
-pip install --upgrade requests
-pip install --upgrade pyopenssl
-pip install --upgrade Jinja2
+echo 'pip-compile requirements.in ...'
+pip-compile --output-file requirements.txt requirements.in
 
-echo 'pip freeze...'
-pip freeze > requirements.txt
+echo 'pip install requirements.txt ...'
+pip install -r requirements.txt
 
-# echo 'pip install from requirements.txt...'
-# pip install -r requirements.txt
+pip list --format=columns
 
 echo 'done'
