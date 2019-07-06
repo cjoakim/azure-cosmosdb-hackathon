e74e1932e02ad95fbdccce909e98b47cec7b127a
Chris Joakim <christopher.joakim@gmail.com>
2019-07-06 10:00:24 -0400

    gs

---
+++ solutions/functions-node/dotnet_core_test_client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_core_test_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
