0d7a529210645c680af484f4b378745e0952278a
Chris Joakim <christopher.joakim@gmail.com>
2019-07-06 06:49:57 -0400

    Deleted the functions/ dir, now in solutions/functions-node

--- functions/hackathon/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Bash shell script to deploy the app to Azure
-# Chris Joakim, Microsoft, 2019/06/14
-
-func azure functionapp publish cjoakim-functions-js
-
-echo 'done'
--- functions/hackathon/local_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Bash shell script to start the local Azure Functions runtime.
-# Chris Joakim, Microsoft, 2019/06/14
-
-func host start
-
-echo 'done'
