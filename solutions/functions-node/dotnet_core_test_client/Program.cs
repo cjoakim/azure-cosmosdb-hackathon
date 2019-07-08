e1b7dbcb2f3da40ad58f78d7a0e191be73d0bd27
Chris Joakim <christopher.joakim@gmail.com>
2019-07-08 06:54:55 -0400

    QueryByPkId

--- solutions/functions-node/dotnet_core_test_client/Program.cs
+++ solutions/functions-node/dotnet_core_test_client/Program.cs
@@ -19,7 +19,7 @@ using Newtonsoft.Json.Serialization;
 // DotNet Core Text Client Console App for the Azure Functions
 // It is used to send messages to Azure Event Hubs, and query
 // the resulting documents in Azure CosmosDB.
-// Chris Joakim, Microsoft, 2019/07/06
+// Chris Joakim, Microsoft, 2019/07/08
 
 namespace dotnet_core_test_client
 {
@@ -27,12 +27,12 @@ namespace dotnet_core_test_client
     {
         private static EventHubClient eventHubClient = null;
         private static DocumentClient cosmosClient   = null;
-        private static string eventHubName     = Environment.GetEnvironmentVariable("AZURE_EVENTHUB_HUBNAME");
-        private static string eventHubConnStr  = Environment.GetEnvironmentVariable("AZURE_EVENTHUB_CONN_STRING");
-        private static string cosmosUri        = Environment.GetEnvironmentVariable("AZURE_COSMOSDB_SQLDB_URI");
-        private static string cosmosKey        = Environment.GetEnvironmentVariable("AZURE_COSMOSDB_SQLDB_KEY");
-        private static string cosmosDbName     = "dev";    // Environment.GetEnvironmentVariable("AZURE_COSMOSDB_SQLDB_DBNAME");
-        private static string cosmosCollName   = "events"; // Environment.GetEnvironmentVariable("AZURE_COSMOSDB_SQLDB_COLLNAME");
+        private static string eventHubName    = Environment.GetEnvironmentVariable("AZURE_EVENTHUB_HUBNAME");
+        private static string eventHubConnStr = Environment.GetEnvironmentVariable("AZURE_EVENTHUB_CONN_STRING");
+        private static string cosmosUri       = Environment.GetEnvironmentVariable("AZURE_COSMOSDB_SQLDB_URI");
+        private static string cosmosKey       = Environment.GetEnvironmentVariable("AZURE_COSMOSDB_SQLDB_KEY");
+        private static string cosmosDatabaseName   = "dev";    // Environment.GetEnvironmentVariable("AZURE_COSMOSDB_SQLDB_DBNAME");
+        private static string cosmosCollectionName = "events"; // Environment.GetEnvironmentVariable("AZURE_COSMOSDB_SQLDB_COLLNAME");
         private static AirportData airportData = new AirportData();
 
         static void Main(string[] args)
@@ -71,6 +71,9 @@ namespace dotnet_core_test_client
             Log("  dotnet run send_event_hub_messsages 10");
             Log("  dotnet run query_cosmos events_for_airport SYD");
             Log("  dotnet run query_cosmos all_events");
+            Log("  dotnet run query_cosmos doc_by_pk_id SYD 047f35b4-7a09-4312-afe1-c44d171606ca");
+
+            
             Log("  dotnet run insert_cosmos_documents 10");
             Log("");
         }
@@ -116,18 +119,51 @@ namespace dotnet_core_test_client
         private static void QueryCosmos(string[] args)
         {
             string queryName = args[1];
-            Log("QueryCosmos: " + queryName + ", " + cosmosDbName + ", " + cosmosCollName);
+            Log("QueryCosmos: " + queryName + ", " + cosmosDatabaseName + ", " + cosmosCollectionName);
             cosmosClient = new DocumentClient(new Uri(cosmosUri), cosmosKey);
+            string iataCode = null;
+            // string city = null;
+            string pk = null;
+            string id = null;
+            // double lat = 0.0;
+            // double lng = 0.0;
+            // double meters = 0.0;
+
   
             switch (queryName)
             {
                 case "events_for_airport":
-                    string iataCode = args[2];
+                    iataCode = args[2];
                     DisplayDocuments(QueryEventsForAirport(iataCode));
                     break;
                 case "all_events":
                     DisplayDocuments(QueryAllEvents());
                     break;
+
+                case "doc_by_pk_id":
+                    pk = args[2];
+                    id = args[3];
+                    ResourceResponse<Document> result = QueryByPkId2(pk, id);
+                    Console.WriteLine(result.Resource);
+                    Console.WriteLine("Charge: " + result.RequestCharge);
+                    break;
+
+
+                // case "doc_by_pk":
+                //     pk = args[2];
+                //     QueryByPk(pk);
+                //     break;
+                // case "doc_by_city":
+                //     city = args[2];
+                //     DisplayDocuments(QueryByCity(city));
+                //     break;
+                // case "docs_by_location":
+                //     lat = args[2];
+                //     lng = args[3];
+                //     meters = args[4];
+                //     DisplayDocuments(QueryByLocation(lat, lng, meters));
+                //     break;
+
                 default:
                     Log("Unknown queryName: " + queryName);
                     DisplayCommandLineExamples();
@@ -150,7 +186,7 @@ namespace dotnet_core_test_client
             string sql = $"SELECT * FROM functions WHERE functions.pk = '{iataCode}'";
             Log("SQL: " + sql);
             IQueryable<Object> query = cosmosClient.CreateDocumentQuery<Object>(
-                CollectionUri(), sql, DefaultFeedOptions());
+                CollectionUri(), sql, StandardFeedOptions());
 
             foreach (Object obj in query)
             {
@@ -163,24 +199,24 @@ namespace dotnet_core_test_client
         {
             List<object> objects = new List<object>();
             string sql = $"SELECT * FROM c WHERE c.pk = '{iataCode}'";
-            return DocumentQuery(sql).Result;        
+            return DocumentsQuery(sql).Result;        
         }
 
         private static List<Object> QueryAllEvents()
         {
             List<object> objects = new List<object>();
             string sql = $"SELECT * FROM c";
-            return DocumentQuery(sql).Result;        
+            return DocumentsQuery(sql).Result;        
         }
 
-        private static async Task<List<object>> DocumentQuery(string sql)
+        private static async Task<List<object>> DocumentsQuery(string sql)
         {
             List<object> objects = new List<object>();
             try
             {
-                Log("DocumentQuery SQL: " + sql);
+                Log("DocumentsQuery SQL: " + sql);
                 var query = cosmosClient.CreateDocumentQuery<Object>(
-                    CollectionUri(), sql, DefaultFeedOptions()).AsDocumentQuery();
+                    CollectionUri(), sql, StandardFeedOptions()).AsDocumentQuery();
                 while (query.HasMoreResults)
                 {
                     foreach (Object obj in await query.ExecuteNextAsync())
@@ -191,10 +227,43 @@ namespace dotnet_core_test_client
             }
             catch (Exception exception)
             {
-                Console.WriteLine($"DocumentQuery {DateTime.Now} > Exception: {exception.Message}");
+                Console.WriteLine($"DocumentsQuery {DateTime.Now} > Exception: {exception.Message}");
             }
             return objects;
         }
+
+        // private static async Task<ResourceResponse<Document>> QueryByPk(string pk) 
+        // {
+        //     return await client.ReadDocumentAsync(CollectionUri(), FeedOptionsWithPk(pk)); 
+        // }
+
+        // private static async Task QueryByPkId0(string pk, string id)
+        // {
+        //     var response = await cosmosClient.ReadDocumentAsync(DocumentUri(id), RequestOptionsWithPk(pk)); 
+        //     Console.WriteLine(response.Resource);
+        //     Console.WriteLine("RequestCharge: " + response.RequestCharge);
+        // }
+
+        private static ResourceResponse<Document> QueryByPkId(string pk, string id)
+        {
+            return cosmosClient.ReadDocumentAsync(DocumentUri(id), RequestOptionsWithPk(pk)).Result; 
+        }
+
+        private static ResourceResponse<Document> QueryByPkId(string pk, string id)
+        {
+            return cosmosClient.ReadDocumentAsync(DocumentUri(id), RequestOptionsWithPk(pk)).Result; 
+        }
+        
+        // }
+        // private static void QueryByCity(string city)
+        // {
+
+        // }
+        // private static void QueryByLocation(double lat, double lng, double meters)
+        // {
+        // }
+
+
 
         private static void InsertCosmosDocuments(int messageCount)
         {
@@ -229,14 +298,38 @@ namespace dotnet_core_test_client
             }
         }
 
-        private static FeedOptions DefaultFeedOptions()
+        private static FeedOptions StandardFeedOptions()
         {
-            return new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true};
+            return new FeedOptions {
+                MaxItemCount = -1, 
+                EnableCrossPartitionQuery = true
+            };
+        }
+
+        private static FeedOptions FeedOptionsWithPk(string pk)
+        {
+            return new FeedOptions {
+                MaxItemCount = -1, 
+                EnableCrossPartitionQuery = false,
+                PartitionKey = new PartitionKey(pk)
+            };
+        }
+
+        private static RequestOptions RequestOptionsWithPk(string pk)
+        {
+            return new RequestOptions {
+                PartitionKey = new PartitionKey(pk)
+            };
         }
 
         private static Uri CollectionUri()
         {
-            return UriFactory.CreateDocumentCollectionUri(cosmosDbName, cosmosCollName);
+            return UriFactory.CreateDocumentCollectionUri(cosmosDatabaseName, cosmosCollectionName);
+        }
+
+        private static Uri DocumentUri(string documentId)
+        {
+            return UriFactory.CreateDocumentUri(cosmosDatabaseName, cosmosCollectionName, documentId);
         }
 
         private static long CurrentEpochTime()
