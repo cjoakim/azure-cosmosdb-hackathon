52cde2f7d821b59e285b8bcb86d88c31aa1f1bb7
Chris Joakim <christopher.joakim@gmail.com>
2019-08-05 15:10:10 -0400

    queries.sh with MIA and MAD

--- solutions/functions-node/dotnet_core_test_client/queries.sh
+++ solutions/functions-node/dotnet_core_test_client/queries.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Bash shell script to execute dotnet to query CosmosDB in several ways.
-# Chris Joakim, Microsoft, 2019/07/08
+# Chris Joakim, Microsoft, 2019/08/05
 
 fromEpoch=0
+MIA_ID="6d61f28f-a796-45ea-8025-355e4a35bd39"
 
 rm tmp/query*.*
 
@@ -13,20 +14,20 @@ echo 'time_now ...'
 dotnet run time_now
 
 echo 'query_cosmos_doc_by_pk_id ...'
-dotnet run query_cosmos doc_by_pk_id SYD e2c0316e-547c-472c-bcfd-60294dd68a0c > tmp/query_cosmos_doc_by_pk_id.txt
+dotnet run query_cosmos doc_by_pk_id MIA 6d61f28f-a796-45ea-8025-355e4a35bd39 > tmp/query_cosmos_doc_by_pk_id.txt
 
 echo 'query_cosmos_all_events ...'
 dotnet run query_cosmos all_events $fromEpoch > tmp/query_cosmos_all_events.txt
 
-echo 'query_cosmos_events_for_airport SYD and CLT ...'
-dotnet run query_cosmos events_for_airport SYD $fromEpoch > tmp/query_cosmos_events_for_airport_SYD.txt
-dotnet run query_cosmos events_for_airport CLT $fromEpoch > tmp/query_cosmos_events_for_airport_CLT.txt
+echo 'query_cosmos_events_for_airport MIA and MAD ...'
+dotnet run query_cosmos events_for_airport MIA $fromEpoch > tmp/query_cosmos_events_for_airport_MIA.txt
+dotnet run query_cosmos events_for_airport MAD $fromEpoch > tmp/query_cosmos_events_for_airport_MAD.txt
 
-echo 'query_cosmos_events_for_city Sydney and Charlotte ...'
-dotnet run query_cosmos events_for_city Sydney  $fromEpoch > tmp/query_cosmos_events_for_city_Sydney.txt
-dotnet run query_cosmos events_for_city Charlotte $fromEpoch > tmp/query_cosmos_events_for_city_Charlotte.txt
+echo 'query_cosmos_events_for_city Miami and Madrid ...'
+dotnet run query_cosmos events_for_city Miami  $fromEpoch > tmp/query_cosmos_events_for_city_Miami.txt
+dotnet run query_cosmos events_for_city Madrid $fromEpoch > tmp/query_cosmos_events_for_city_Madrid.txt
 
 echo 'query_cosmos_events_for_location ...'
-dotnet run query_cosmos events_for_location -80.842842 35.499586 1 $fromEpoch > tmp/query_cosmos_events_for_location.txt
+dotnet run query_cosmos events_for_location -80.290556 25.79325  1 $fromEpoch > tmp/query_cosmos_events_for_location.txt
 
 echo 'done'
